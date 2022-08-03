cb8f1dac38d19c53121c26e150e613f0589e5916
n-f9 <ndferguson@protonmail.com>
2022-08-02 20:52:57 -0400

    pug, ts, and scss

---
+++ .prettierrc.js
@@ -0,0 +1,22 @@
+// @ts-check
+/// <reference types="@prettier/plugin-pug/src/prettier" />
+
+/**
+ * @type {import('prettier').Options}
+ */
+module.exports = {
+	plugins: [require.resolve('@prettier/plugin-pug')],
+
+	trailingComma: 'all',
+	singleQuote: true,
+	tabWidth: 4,
+	useTabs: true,
+	semi: false,
+	bracketSpacing: true,
+	arrowParens: 'always',
+	printWidth: 200,
+	pugSingleQuote: 'true',
+	pugAttributeSeparator: 'always',
+	pugClassNotation: 'attribute',
+	pugIdNotation: 'as-is',
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{html,js,ts,jsx,tsx}'],
+	content: ['./src/**/*.{html,js,ts,jsx,tsx,pug}'],
 	theme: {
 		extend: {},
 	},
